bef4e34aa61503b53ddd4865a14ab777af650163
Margaren <margarita.kovaleva30@mail.ru>
2024-01-13 00:58:38 +0600

    Add files via upload

---
+++ TxtFIleRead/ReaderClass.cs
@@ -0,0 +1,18 @@
+﻿namespace TxtFIleRead
+{
+    public static class ReaderClass
+    {
+        public static string[] Read(string file)
+        {
+            var fileName = file;
+            var dataAsArray = File.ReadAllLines(fileName);
+            return dataAsArray;
+        }
+        public static void Write(string[] str)
+        {
+            var fileNameArray = @"E:\aip\Spaceships\Lab_6\Lab_6_Out.txt";
+            var dataArray = str;
+            File.WriteAllLines(fileNameArray, dataArray);
+        }
+    }
+}
