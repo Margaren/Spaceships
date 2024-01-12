06adee978d4df17384c668a6a31809099113859f
Margaren <margarita.kovaleva30@mail.ru>
2024-01-13 00:57:47 +0600

    Add files via upload

---
+++ Tester/Program.cs
@@ -0,0 +1,17 @@
+﻿using SpaceFleetDispatcher;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //E:\aip\Spaceships\Information.txt
+            Console.WriteLine("Введите расположение файла:");
+            string location = Console.ReadLine();
+            Dispatcher.SetDataAndGetOutData(location);
+            Console.WriteLine("Программа завершена успешно");
+            Console.ReadLine();
+        }
+    }
+}
